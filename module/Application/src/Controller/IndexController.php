bf9991bff7aa83125315fa351442ef56adca894c
Benoit Duval <benoit.duval.pro@gmail.com>
2016-08-02 18:18:18 +0200

    skeleton + model

---
+++ module/Application/src/Controller/IndexController.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
+ * @copyright Copyright (c) 2005-2016 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ */
+
+namespace Application\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+use Application\Model\AlbumTable;
+
+class IndexController extends AbstractActionController
+{
+    private $table;
+
+    public function __construct(AlbumTable $table)
+    {
+        $this->table = $table;
+    }
+
+    public function indexAction()
+    {
+        return new ViewModel([
+            'albums' => $this->table->fetchAll(),
+        ]);
+    }
+
+    public function addAction()
+    {
+        return new ViewModel();
+    }
+
+    public function editAction()
+    {
+        return new ViewModel();
+    }
+
+    public function deleteAction()
+    {
+        return new ViewModel();
+    }
+
+}
