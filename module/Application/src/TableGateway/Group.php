17c4f31d761b1e9b683d3e305fe6f809453cf823
Benoit Duval <benoit.duval.pro@gmail.com>
2016-11-25 15:53:34 +0100

    admin emails

--- module/Application/src/TableGateway/Group.php
+++ module/Application/src/TableGateway/Group.php
@@ -5,6 +5,7 @@ namespace Application\TableGateway;
 use RuntimeException;
 use Zend\Db\TableGateway\TableGatewayInterface;
 use Application\TableGateway;
+use Application\Model;
 
 class Group extends AbstractTableGateway
 {
@@ -27,5 +28,24 @@ class Group extends AbstractTableGateway
             ]);
         }
         return $groups;
+    }
+
+    public function getAdmins($groupId)
+    {
+        $userGroupTable = $this->getContainer()->get(TableGateway\UserGroup::class);
+        $objs = $userGroupTable->fetchAll(['groupId' => $groupId, 'admin' => Model\UserGroup::ADMIN]);
+        $users = [];
+
+        $userTable = $this->getContainer()->get(TableGateway\User::class);
+        if ($objs->toArray()) {
+            $ids = [];
+            foreach ($objs as $obj) {
+                $ids[] = $obj->userId;
+            }
+            $users = $userTable->fetchAll([
+                'id' => $ids
+            ]);
+        }
+        return $users;
     }
 }
