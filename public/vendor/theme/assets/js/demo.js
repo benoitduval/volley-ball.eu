be18e5b94f8ed5dd11615a015c1eae7337b711ba
Benoit Duval <benoit.duval.pro@gmail.com>
2017-10-24 16:00:54 +0200

    modal event links

--- public/vendor/theme/assets/js/demo.js
+++ public/vendor/theme/assets/js/demo.js
@@ -39,7 +39,7 @@ demo = {
                 zoom: 13,
                 center: myLatlng,
                 scrollwheel: false, //we disable de scroll over the map, it is a really annoing when you scroll through page
-                disableDefaultUI: true, // a way to quickly hide all controls
+                disableD6efaultUI: true, // a way to quickly hide all controls
                 zoomControl: true,
                 styles: [{"featureType":"water","stylers":[{"saturation":43},{"lightness":-11},{"hue":"#0088ff"}]},{"featureType":"road","elementType":"geometry.fill","stylers":[{"hue":"#ff0000"},{"saturation":-100},{"lightness":99}]},{"featureType":"road","elementType":"geometry.stroke","stylers":[{"color":"#808080"},{"lightness":54}]},{"featureType":"landscape.man_made","elementType":"geometry.fill","stylers":[{"color":"#ece2d9"}]},{"featureType":"poi.park","elementType":"geometry.fill","stylers":[{"color":"#ccdca1"}]},{"featureType":"road","elementType":"labels.text.fill","stylers":[{"color":"#767676"}]},{"featureType":"road","elementType":"labels.text.stroke","stylers":[{"color":"#ffffff"}]},{"featureType":"poi","stylers":[{"visibility":"off"}]},{"featureType":"landscape.natural","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"color":"#b8cb93"}]},{"featureType":"poi.park","stylers":[{"visibility":"on"}]},{"featureType":"poi.sports_complex","stylers":[{"visibility":"on"}]},{"featureType":"poi.medical","stylers":[{"visibility":"on"}]},{"featureType":"poi.business","stylers":[{"visibility":"simplified"}]}]
 
@@ -519,7 +519,7 @@ demo = {
                         if (typeof event.url !== "undefined") {
                             $('#modal-title').html(myEvent.title);
                             $('#modal-date').html(myEvent.date);
-                            $('#event-url').attr('href',myEvent.url);
+                            $('.event-url').attr('href', myEvent.url);
                             $('#modal-count').html(myEvent.count);
                             $('#modal-place').html(myEvent.place);
                             $('#modal-city').html(myEvent.city);
@@ -528,6 +528,7 @@ demo = {
                             $('#modal-month').html(myEvent.month);
                             $('#modal-day').html(myEvent.day);
                             $('#modal-date').html(myEvent.date);
+                            $('#event-place-url').attr('href', 'https://maps.google.com/?q=' + myEvent.address + '+' + myEvent.city + '+' + myEvent.zipcode);
                             calendarModal.modal();
 
                             var url = '/api/guest/response/' + myEvent.id;
