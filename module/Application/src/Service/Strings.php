07d2e762d1ea9fd69345da304642acd213b076db
Benoit Duval <benoit.duval.pro@gmail.com>
2018-02-08 17:58:25 +0100

    new way to take stats

--- module/Application/src/Service/Strings.php
+++ module/Application/src/Service/Strings.php
@@ -4,14 +4,16 @@ namespace Application\Service;
 
 class Strings
 {
-	public static function toSlug($name)
-	{
-	    $str = strtolower($name);
-	    $str = preg_replace('/ /', '-', $str);
-	    $str = htmlentities($str, ENT_NOQUOTES, 'utf-8');
-	    $str = preg_replace('#&([A-za-z])(?:acute|cedil|caron|circ|grave|orn|ring|slash|th|tilde|uml);#', '\1', $str);
-	    $str = preg_replace('#&([A-za-z]{2})(?:lig);#', '\1', $str); // pour les ligatures e.g. '&oelig;'
-	    $str = preg_replace('#&[^;]+;#', '', $str); // supprime les autres caractères
-	    return $str;
-	}
+    public static function toSlug($name)
+    {
+        $str = strtolower($name);
+        $str = preg_replace('/ /', '-', $str);
+        $str = preg_replace('/', '-', $str);
+        $str = preg_replace('\\', '-', $str);
+        $str = htmlentities($str, ENT_NOQUOTES, 'utf-8');
+        $str = preg_replace('#&([A-za-z])(?:acute|cedil|caron|circ|grave|orn|ring|slash|th|tilde|uml);#', '\1', $str);
+        $str = preg_replace('#&([A-za-z]{2})(?:lig);#', '\1', $str); // pour les ligatures e.g. '&oelig;'
+        $str = preg_replace('#&[^;]+;#', '', $str); // supprime les autres caractères
+        return $str;
+    }
 }
--- public/js/script.js
+++ public/js/script.js
@@ -459,10 +459,12 @@ demo = {
     },
 
     initSubmitButton: function () {
-        $('input[type="submit"]').each(function () {
-            $(this).on('click', function() {
-                $(this).attr('value', '<i class="fa fa-spinner fa-spin"></i>');
-            });
+        $('.btn-wait').on('click', function() {
+            var $this = $(this);
+            $this.button('loading');
+            setTimeout(function() {
+                $this.button('reset');
+            }, 500);
         });
     },
 
@@ -615,6 +617,56 @@ demo = {
                         useHTML: true
                     }
                 });
+            });
+        });
+    },
+
+    initWizard: function(){
+        $(document).ready(function(){
+
+            var $validator = $("#wizardForm").validate();
+
+            // you can also use the nav-pills-[blue | azure | green | orange | red] for a different color of wizard
+            $('#wizardCard').bootstrapWizard({
+                tabClass: 'nav nav-pills',
+                nextSelector: '.btn-next',
+                previousSelector: '.btn-back',
+                onInit : function(tab, navigation, index){
+
+                    //check number of tabs and fill the entire row
+                    var $total = navigation.find('li').length;
+                    $width = 100/$total;
+
+                    $display_width = $(document).width();
+
+                    if($display_width < 600 && $total > 3){
+                       $width = 50;
+                    }
+
+                    navigation.find('li').css('width',$width + '%');
+                },
+                onTabClick : function(tab, navigation, index){
+                    // Disable the posibility to click on tabs
+                    return true;
+                },
+                onTabShow: function(tab, navigation, index) {
+                    var $total = navigation.find('li').length;
+                    var $current = index+1;
+
+                    var wizard = navigation.closest('.card-wizard');
+
+                    // If it's the last tab then hide the last button and show the finish instead
+                    if($current >= $total) {
+                        $(wizard).find('.btn-next').hide();
+                        $(wizard).find('.btn-finish').show();
+                    } else if($current == 1){
+                        $(wizard).find('.btn-back').hide();
+                    } else {
+                        $(wizard).find('.btn-back').show();
+                        $(wizard).find('.btn-next').show();
+                        $(wizard).find('.btn-finish').hide();
+                    }
+                }
             });
         });
     }
